0ef99ddf70613987129fc934232f046f3116038a
Mogakamo <mogaka.amo254@gmail.com>
2022-01-17 11:54:40 +0300

    Finished the LargeCard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
@@ -44,7 +45,12 @@ export default function Home({ exploreData, cardsData }) {
         </section>
 
         <section>
-          
+          <LargeCard 
+            img="https://links.papareact.com/4cj"
+            title="The Greatest Outdoors"
+            description="Wishlist curated by Airbnb."
+            buttonText="Get Inspired"
+          />
         </section>
       </main>
     </div>
