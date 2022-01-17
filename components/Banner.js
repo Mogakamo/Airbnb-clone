247a025811485b6ed46c842a996e22d81893e418
Mogakamo <mogaka.amo254@gmail.com>
2022-01-17 10:15:49 +0300

    finished with the banner

--- components/Banner.js
+++ components/Banner.js
@@ -9,8 +9,10 @@ function Banner() {
         objectFit="cover"
         layout="fill"
       />
-      <div>
-          <p>Not sure where to start</p>
+      <div className="absolute top-1/2 w-full text-center">
+          <p className="text-sm sm:text-lg">Not sure where to go? Perfect</p>
+          <button type="" className="bg-white border-2 text-purple-500 px-10 py-4 shadow-md rounded-full font-bold my-3
+                hover:shadow-xl active:scale-90 transition duration-150 ease-out">I'm flexible</button>
       </div>
     </div>
   );
