7c5230ab51dbad953d7b2ada8b90e25eb76600fd
Mogakamo <mogaka.amo254@gmail.com>
2022-01-17 12:12:47 +0300

    Finished with the UI

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 bg-gray-100 text-gray-600 gap-y-10 px-32 py-14">
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>New Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>New Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>New Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>New Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div>&copy; 2022 | All Rights Reserved</div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
