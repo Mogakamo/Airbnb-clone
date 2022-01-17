d08051de6e44302c38baf48fca657bb9d65815b1
Mogakamo <mogaka.amo254@gmail.com>
2022-01-17 11:40:26 +0300

    Created the LargeCard component

---
+++ components/LargeCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LargeCard({img, title,  description, buttonText}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LargeCard
+{}
